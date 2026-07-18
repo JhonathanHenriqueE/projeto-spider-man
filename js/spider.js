d86d33260e1625f5cdc7d2c8534e6bafd507902a
Jhonathan Henrique Eufrasio Rufino <jhonathanhenrique1718@gmail.com>
2026-07-17 23:54:01 -0300

    projeto spider-man

---
+++ js/spider.js
@@ -0,0 +1,16 @@
+function trocarTobey() {
+    document.getElementById('texto-t').innerHTML = '<ul> <li> Nome do ator: Tobey Maguire </li> <li> idade: 51 anos </li> <li> Nacionalidade: Norte-Americano </li> </ul>' ; 
+    document.getElementById('img-tob').src = "img/tobey.jpeg";
+    
+}
+
+function trocarAndrew(){
+    document.getElementById('texto-a').innerHTML = '<ul> <li> Nome do ator: Andrew Garfield </li> <li> idade: 43 anos </li> <li> Nacionalidade: Britânico-Americano </li> </ul>' ; 
+     document.getElementById('img-and').src = "img/andrew.jpeg"
+
+}
+
+function trocarTom(){
+    document.getElementById('texto-tom').innerHTML = '<ul> <li> Nome do ator: Tom Holland  </li> <li> idade: 30 anos </li> <li> Nacionalidade: Britânico </li> </ul>' ; 
+     document.getElementById('img-tom').src = "img/tom.jpeg"
+}
